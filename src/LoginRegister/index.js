d27554b48662f8ec5114197e6ef25314c8eadb57
Randy Gohmann <rgohma2@gmail.com>
2020-02-27 15:31:38 -0600

    created login/register form ans it changing input changes state

--- src/LoginRegister/index.js
+++ src/LoginRegister/index.js
@@ -16,8 +16,23 @@ class LoginRegister extends React.Component {
 		super(props)
 
 		this.state = {
-
+			first_name: '',
+			last_name: '',
+			email: '',
+			password: '',
+			address_1: '',
+			address_2: '',
+			city: '', 
+			state: '', 
+			zip_code: '', 
+			country: '',
 		}
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
 	}
 
 	render() {
@@ -35,8 +50,8 @@ class LoginRegister extends React.Component {
 						<div className='form'>
 							<Segment
 							style={{
-								height: '200px',
-								boxShadow: '5px 5px 5px 2px black'
+								height: '65%',
+								boxShadow: '0px 8px 15px -6px black'
 							}}
 							>
 							<Grid.Row
@@ -48,18 +63,115 @@ class LoginRegister extends React.Component {
 							</Grid.Row>
 							<Grid.Row>
 								<Form>
+								<Form.Field>
+								<div
+								style={{
+									display:'flex'
+								}}
+								>
+								<Grid.Column style={{marginRight:'20px'}}>
+									<Segment>
 									<Form.Group>
+										<Form.Input 
+										label="First Name" 
+										type="text" 
+										name="first_name"
+										value={this.state.first_name}
+										onChange={this.handleChange}
+										/>
+
+										<Form.Input 
+										label="Last Name"
+										type="text" 
+										name="last_name"
+										value={this.state.last_name}
+										onChange={this.handleChange}
+										/>
+									</Form.Group>
 										<Form.Input 
 										label="Email" 
 										type="text" 
-										name="email"/>
+										name="email"
+										value={this.state.email}
+										onChange={this.handleChange}
+										/>
 
 										<Form.Input 
 										label="Password"
 										type="password" 
 										name="password"
+										value={this.state.password}
+										onChange={this.handleChange}
+										/>
+									</Segment>
+								</Grid.Column>
+								<Grid.Column>
+									<Segment
+									style={{
+										paddingBottom: '0'
+									}}
+									>
+									<Form.Group>
+										<Form.Input 
+										label="Address 1" 
+										type="text" 
+										name="address_1"
+										value={this.state.address_1}
+										onChange={this.handleChange}
+										/>
+
+										<Form.Input 
+										label="Address 2"
+										type="text" 
+										name="address_2"
+										value={this.state.address_2}
+										onChange={this.handleChange}
 										/>
 									</Form.Group>
+									<Form.Group>
+										<Form.Input 
+										label="City" 
+										type="text" 
+										name="city"
+										value={this.state.city}
+										onChange={this.handleChange}
+										/>
+
+										<Form.Input 
+										label="State"
+										type="text" 
+										name="state"
+										value={this.state.state}
+										onChange={this.handleChange}
+										/>
+									</Form.Group>
+									<Form.Group>
+										<Form.Input 
+										label="Zip Code" 
+										type="text" 
+										name="zip_code"
+										value={this.state.zip_code}
+										onChange={this.handleChange}
+										/>
+
+										<Form.Input 
+										label="Country"
+										type="text" 
+										name="country"
+										value={this.state.country}
+										onChange={this.handleChange}
+										/>
+									</Form.Group>
+									</Segment>
+								</Grid.Column>
+								</div>
+								<Button
+								style={{
+									width:'100%',
+									marginTop: '10px'
+								}}
+								>Register</Button>
+								</Form.Field>
 								</Form>
 							</Grid.Row>
 								<small> Already have an account? Login <Link to='/'>here</Link>!</small>
@@ -70,8 +182,8 @@ class LoginRegister extends React.Component {
 						<div className='form'>
 							<Segment
 							style={{
-								height: '200px',
-								boxShadow: '5px 5px 5px 2px black'
+								height: '250px',
+								boxShadow: '0px 8px 15px -6px black'
 							}}
 							>
 							<Grid.Row
@@ -83,18 +195,30 @@ class LoginRegister extends React.Component {
 							</Grid.Row>
 							<Grid.Row>
 								<Form>
+								<Segment>
 									<Form.Group>
 										<Form.Input 
 										label="Email" 
 										type="text" 
-										name="email"/>
+										name="email"
+										value={this.state.email}
+										onChange={this.handleChange}
+										/>
 
 										<Form.Input 
 										label="Password"
 										type="password" 
 										name="password"
+										value={this.state.password}
+										onChange={this.handleChange}
 										/>
 									</Form.Group>
+									<Button
+									style={{
+										width:'100%'
+									}}
+									>Login</Button>
+								</Segment>
 								</Form>
 							</Grid.Row>
 								<small> Don't have an account yet? Register <Link to='/register'>here</Link></small>
